d2edce791258c1e2f62cd47c33434022dd03c3c5
seebawaih551 <seebawaih551@gmail.com>
2023-08-08 03:58:25 +0400

    validate data

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -11,19 +11,58 @@ class AddNoteBottomSheet extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(children: [
-          SizedBox(height: 32),
-          CostumTextField(hint: 'title'),
-          SizedBox(height: 16),
-          CostumTextField(
-            hint: 'contents',
-            maxLines: 5,
-          ),
-          SizedBox(height: 50),
-          CostumButtun(),
-          SizedBox(height: 16),
-        ]),
+        child: AddNoteForm(),
       ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(children: [
+        const SizedBox(height: 32),
+        CostumTextField(
+            onSaved: (value) {
+              title = value;
+            },
+            hint: 'title'),
+        const SizedBox(height: 16),
+        CostumTextField(
+          onSaved: (value) {
+            subTitle = value;
+          },
+          hint: 'contents',
+          maxLines: 5,
+        ),
+        const SizedBox(height: 50),
+        CostumButtun(
+          onTap: () {
+            if (formKey.currentState!.validate()) {
+              formKey.currentState!.save();
+            } else {
+              autovalidateMode = AutovalidateMode.always;
+              setState(() {});
+            }
+          },
+        ),
+        const SizedBox(height: 16),
+      ]),
     );
   }
 }
--- lib/widgets/costum_button_widget.dart
+++ lib/widgets/costum_button_widget.dart
@@ -3,22 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constance.dart';
 
 class CostumButtun extends StatelessWidget {
-  const CostumButtun({super.key});
+  const CostumButtun({super.key, this.onTap});
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      child: const Center(
-          child: Text(
-        'Add',
-        style: TextStyle(
-            color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
-      )),
-      decoration: BoxDecoration(
-        color: kPrimaryColor,
-        borderRadius: BorderRadius.circular(16),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        child: const Center(
+            child: Text(
+          'Add',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        )),
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(16),
+        ),
       ),
     );
   }
