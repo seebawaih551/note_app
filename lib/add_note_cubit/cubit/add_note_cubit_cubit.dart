9ea6a1586a184db77fa8c66d4fb7dfe85f5e144e
seebawaih551 <seebawaih551@gmail.com>
2023-08-08 22:18:44 +0400

    provide add note cubit

--- lib/add_note_cubit/cubit/add_note_cubit_cubit.dart
+++ lib/add_note_cubit/cubit/add_note_cubit_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constance.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_cubit_state.dart';
@@ -7,5 +9,15 @@ part 'add_note_cubit_state.dart';
 class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
   AddNoteCubitCubit() : super(AddNoteCubitInitial());
 
-  addNote(NoteModel note) {}
+  addNote(NoteModel note) async {
+    emit(AddNoteCubitLoading());
+    try {
+      var noteBox = Hive.box(kNotesBox);
+      emit(AddNoteCubitSuccess());
+      await noteBox.add(note);
+    } catch (e) {
+      AddNoteCubitFailure(e.toString());
+      //
+    }
+  }
 }
